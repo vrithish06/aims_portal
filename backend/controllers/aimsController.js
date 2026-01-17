09c71c11dfabc2afe8d781316cf96caf6ce28f24
Venkata Rithish Reddy Chamala <vrithishreddy06@Venkatas-MacBook-Air.local>
2026-01-17 11:43:58 +0530

    changed aimscontroller.js

--- backend/controllers/aimsController.js
+++ backend/controllers/aimsController.js
@@ -1,6 +1,7 @@
 import supabase from "../config/db.js";
 import bcrypt from "bcrypt";
 
+//help endpoint
 export const getHelp = async (req, res) => {
   res.status(200).json({
     message: "This is help endpoint from aimsController",
@@ -8,8 +9,6 @@ export const getHelp = async (req, res) => {
       "https://docs.google.com/document/d/e/2PACX-1vR4cB5IKv1YEAiZUgKt4ZnzznaMdeG-VjIU_NBmxzej54YWlHEMwuHK4l2JzXPiyJAteAsxbkSPugFE/pub"
   });
 };
-
-
 //creating the user by admin
 export const createUser = async (req, res) => {
   const {
@@ -17,7 +16,8 @@ export const createUser = async (req, res) => {
     password,
     first_name,
     last_name,
-    role
+    role,
+    branch
   } = req.body;
 
   if (!email || !password || !role) {
@@ -28,40 +28,39 @@ export const createUser = async (req, res) => {
   }
 
   try {
+    // hash password
     const password_hashed = await bcrypt.hash(password, 10);
+
     const { data, error } = await supabase
       .from("user")
       .insert({
-        is_deleted: false,
-        txn_no: 1,
-        ins_ts: new Date(),
-        upd_ts: new Date(),
-        password_hashed,
         email,
+        password_hashed,
         first_name,
         last_name,
-        is_locked: false,
-        role
+        role,
+        branch
       })
       .select()
       .single();
 
     if (error) throw error;
 
-    res.status(201).json({
+    return res.status(201).json({
       success: true,
       data
     });
 
   } catch (err) {
-    console.error("Error creating user:", err);
-    res.status(500).json({
+    console.error("createUser error:", err);
+
+    return res.status(500).json({
       success: false,
-      message: "Server error"
+      message: err.message
     });
   }
 };
-
+//creating course
 export const createCourse = async (req, res) => {
   const {
     code,
@@ -113,96 +112,5 @@ export const createCourse = async (req, res) => {
     });
   }
 };
-
-export const getProducts=async (req,res) =>{
-    try{
-        const products=await sql`SELECT * FROM products ORDER BY created_at DESC`;
-        res.status(200).json({success:true,data:products});
-
-    }
-    catch(err){
-        res.status(500).json({success:false,message:"Server Error"});   
-        console.log("error in getPRoduct function",err);
-
-    }
-};
-
-export const createProducts = async (req, res) => {
-  const { name } = req.body;
-
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide name" });
-  }
-
-  try {
-    const { data, error } = await supabase
-      .from("test_table")
-      .insert([{ name }])
-      .select()
-      .single();
-
-    if (error) {
-      throw error;
-    }
-
-    res.status(201).json({
-      success: true,
-      data: data
-    });
-  } catch (err) {
-    console.error("error in createProducts function", err);
-    res.status(500).json({
-      success: false,
-      message: "Server Error"
-    });
-  }
-};
-
-export const getProduct = async (req,res) =>{
-    const {id}=req.params;
-    try{
-        const existingProduct=await sql`SELECT * FROM products WHERE id=${id}`;
-        res.status(200).json({success:true,data:existingProduct[0]});
-    }
-    catch(err){
-        res.status(500).json({success:false,message:"Server Error"});   
-        console.log("error in getProduct function",err);
-    }
-};
-export const updateProduct = async (req, res) => {
-    const { id } = req.params;
-    const { name, image, price } = req.body;
-
-    try {
-        const updatedProduct = await sql`
-            UPDATE products
-            SET name = ${name}, image = ${image}, price = ${price}
-            WHERE id = ${id}
-            RETURNING *
-        `;
-
-        if (updatedProduct.length === 0) {
-            return res.status(404).json({
-                success: false,
-                message: "Product not found"
-            });
-        }
-
-        return res.status(200).json({
-            success: true,
-            data: updatedProduct[0]
-        });
-
-    } catch (err) {
-        console.log("error in updateProduct function", err);
-        return res.status(500).json({
-            success: false,
-            message: "Server Error"
-        });
-    }
-};
-
 
 
